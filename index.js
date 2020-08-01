0e4475c74ad8a020efed0cc80c6290668537b989
Rafael <rafa123839@gmail.com>
2020-08-01 11:56:03 -0300

    comitando site

---
+++ index.js
@@ -0,0 +1,9 @@
+let botaoToggle = document.querySelector('#menu-toggle');
+let navMenu = document.querySelector('#menu');
+
+
+botaoToggle.addEventListener('click', function(){
+
+    navMenu.classList.toggle('active')    
+
+})
